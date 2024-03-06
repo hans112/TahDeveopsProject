29ecf00035e8e082d99eae80fd110048a48ca85b
hans112 <hansdevops127@gmail.com>
2024-03-06 10:53:20 +0100

    testing tah project

--- src/main/java/TahProjectPackage/TahProjectClas.java
+++ src/main/java/TahProjectPackage/TahProjectClas.java
@@ -5,7 +5,7 @@ public class TahProjectClas {
 	public static void main(String[] args) {
 		System.out.println("wellcome in TahDeveopsProject");
            System.out.println("the second commit");
-         
+           System.out.println("JenkinsWithparametere doesn't work test if this project work");
 	}
 
 }
